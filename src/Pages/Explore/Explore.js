4fd39e5c29057092e749afdf14d36690b05b410d
Keyursangani <57138163+dipen2000@users.noreply.github.com>
2022-06-06 17:29:09 +0530

    update explore.js

---
+++ src/Pages/Explore/Explore.js
@@ -0,0 +1,45 @@
+import { BatNetworkContainer } from "../../Components/Wrapper/BatNetworkContainer";
+import { Sidebar } from "../../Components/Sidebar/Sidebar";
+import { RightSidebar } from "../../Components/RightSidebar/RightSidebar";
+import { getPosts, PostCard } from "../../features/post";
+import { getAllUsers } from "../../features/user";
+import { useEffect } from "react";
+import "./Explore.css";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+const Explore = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.post.posts);
+  const isLoading = useSelector((state) => state.post.isLoading);
+
+  useEffect(() => {
+    dispatch(getPosts());
+    dispatch(getAllUsers());
+  }, [dispatch]);
+
+  return (
+    <BatNetworkContainer>
+      <div className="home-grid-container bord-3-purple">
+        <Sidebar />
+        <div className="bord-3-red">
+          <div className="flex-col">
+            <div className="page-title bord-3-black">Explore</div>
+            {isLoading ? (
+              <div>Loading...</div>
+            ) : posts.length ? (
+              [...posts].reverse().map((post) => {
+                return <PostCard key={post._id} post={post} />;
+              })
+            ) : (
+              <div>No posts</div>
+            )}
+          </div>
+        </div>
+        <RightSidebar />
+      </div>
+    </BatNetworkContainer>
+  );
+};
+
+export { Explore };
